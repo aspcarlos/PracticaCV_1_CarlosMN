d3126ad4f317d01f30239739a3be16e07bfd6b61
Carlos Montoro Navas <Carlosmon2004@gmail.com>
2023-03-16 00:06:02 +0100

    Adición de la Estructura de la Clase

--- PracticaCV_1_CarlosMN/Clase_CarlosMN.cs
+++ PracticaCV_1_CarlosMN/Clase_CarlosMN.cs
@@ -8,5 +8,15 @@ namespace PracticaCV_1_CarlosMN
 {
     internal class Clase_CarlosMN
     {
+        // CONSTANTES
+
+        // MIEMBROS PRIVATIVOS
+
+        // CONSTRUCTORES
+
+        // PROPIEDADES
+
+        // METODOS PUBLICOS/PRIVATIVOS
+
     }
 }
