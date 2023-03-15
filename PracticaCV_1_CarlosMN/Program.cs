c6a5ae7f7819fa3750bfe3f7d9e2200a2a79b5ee
Carlos Montoro Navas <Carlosmon2004@gmail.com>
2023-03-15 19:52:33 +0100

    // CONSTANTES // VARIABLES // INICIALIZACION // ENTRADA // PROCESO // SALIDA

--- PracticaCV_1_CarlosMN/Program.cs
+++ PracticaCV_1_CarlosMN/Program.cs
@@ -1,10 +1,1 @@
-﻿namespace PracticaCV_1_CarlosMN
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
